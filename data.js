8c2896bbd2b1d5579beefbed75f5a1c306bb42e6
Michal Michalczuk <michalczukm@gmail.com>
2018-04-07 23:30:50 +0200

    add extra lists for moving lists excercise

--- data.js
+++ data.js
@@ -29,6 +29,294 @@ const STORAGE = {
       'title': 'Things to do tomorrow',
       'purpose': `It's always better to write things down!`,
       'userId': 3
+    },
+    {
+      "id": 6,
+      "title": "Rivas from comp: Tubalum",
+      "purpose": "JustRivas from comp: Tubalum stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 7,
+      "title": "Ward from comp: Centregy",
+      "purpose": "JustWard from comp: Centregy stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 8,
+      "title": "Kennedy from comp: Equitax",
+      "purpose": "JustKennedy from comp: Equitax stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 9,
+      "title": "Burch from comp: Geologix",
+      "purpose": "JustBurch from comp: Geologix stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 10,
+      "title": "Wooten from comp: Insuresys",
+      "purpose": "JustWooten from comp: Insuresys stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 11,
+      "title": "Klein from comp: Visualix",
+      "purpose": "JustKlein from comp: Visualix stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 12,
+      "title": "Sargent from comp: Slofast",
+      "purpose": "JustSargent from comp: Slofast stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 13,
+      "title": "Rios from comp: Quinex",
+      "purpose": "JustRios from comp: Quinex stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 14,
+      "title": "Cruz from comp: Makingway",
+      "purpose": "JustCruz from comp: Makingway stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 15,
+      "title": "Romero from comp: Valpreal",
+      "purpose": "JustRomero from comp: Valpreal stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 16,
+      "title": "Johns from comp: Dreamia",
+      "purpose": "JustJohns from comp: Dreamia stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 17,
+      "title": "Kerr from comp: Maximind",
+      "purpose": "JustKerr from comp: Maximind stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 18,
+      "title": "Mcfadden from comp: Slambda",
+      "purpose": "JustMcfadden from comp: Slambda stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 19,
+      "title": "Ramirez from comp: Niquent",
+      "purpose": "JustRamirez from comp: Niquent stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 20,
+      "title": "Navarro from comp: Realysis",
+      "purpose": "JustNavarro from comp: Realysis stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 21,
+      "title": "Dillon from comp: Unia",
+      "purpose": "JustDillon from comp: Unia stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 22,
+      "title": "Sheppard from comp: Isosure",
+      "purpose": "JustSheppard from comp: Isosure stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 23,
+      "title": "Cameron from comp: Elemantra",
+      "purpose": "JustCameron from comp: Elemantra stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 24,
+      "title": "Albert from comp: Gazak",
+      "purpose": "JustAlbert from comp: Gazak stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 25,
+      "title": "Lindsey from comp: Sarasonic",
+      "purpose": "JustLindsey from comp: Sarasonic stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 26,
+      "title": "Roman from comp: Zilodyne",
+      "purpose": "JustRoman from comp: Zilodyne stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 27,
+      "title": "Rowe from comp: Exotechno",
+      "purpose": "JustRowe from comp: Exotechno stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 28,
+      "title": "Sutton from comp: Motovate",
+      "purpose": "JustSutton from comp: Motovate stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 29,
+      "title": "Dudley from comp: Gushkool",
+      "purpose": "JustDudley from comp: Gushkool stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 30,
+      "title": "Sweet from comp: Geekko",
+      "purpose": "JustSweet from comp: Geekko stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 31,
+      "title": "Atkinson from comp: Telequiet",
+      "purpose": "JustAtkinson from comp: Telequiet stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 32,
+      "title": "Holmes from comp: Bolax",
+      "purpose": "JustHolmes from comp: Bolax stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 33,
+      "title": "Mcgee from comp: Filodyne",
+      "purpose": "JustMcgee from comp: Filodyne stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 34,
+      "title": "Velasquez from comp: Xeronk",
+      "purpose": "JustVelasquez from comp: Xeronk stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 35,
+      "title": "Vega from comp: Exozent",
+      "purpose": "JustVega from comp: Exozent stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 36,
+      "title": "Quinn from comp: Nitracyr",
+      "purpose": "JustQuinn from comp: Nitracyr stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 37,
+      "title": "Mann from comp: Pigzart",
+      "purpose": "JustMann from comp: Pigzart stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 38,
+      "title": "Burks from comp: Prismatic",
+      "purpose": "JustBurks from comp: Prismatic stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 39,
+      "title": "Norris from comp: Bizmatic",
+      "purpose": "JustNorris from comp: Bizmatic stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 40,
+      "title": "Whitehead from comp: Vurbo",
+      "purpose": "JustWhitehead from comp: Vurbo stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 41,
+      "title": "Frazier from comp: Joviold",
+      "purpose": "JustFrazier from comp: Joviold stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 42,
+      "title": "Blackwell from comp: Cablam",
+      "purpose": "JustBlackwell from comp: Cablam stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 43,
+      "title": "Bullock from comp: Cujo",
+      "purpose": "JustBullock from comp: Cujo stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 44,
+      "title": "Pope from comp: Veraq",
+      "purpose": "JustPope from comp: Veraq stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 45,
+      "title": "Silva from comp: Aquoavo",
+      "purpose": "JustSilva from comp: Aquoavo stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 46,
+      "title": "Jefferson from comp: Zomboid",
+      "purpose": "JustJefferson from comp: Zomboid stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 47,
+      "title": "Turner from comp: Lexicondo",
+      "purpose": "JustTurner from comp: Lexicondo stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 48,
+      "title": "Humphrey from comp: Centuria",
+      "purpose": "JustHumphrey from comp: Centuria stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 49,
+      "title": "Hubbard from comp: Talkola",
+      "purpose": "JustHubbard from comp: Talkola stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 50,
+      "title": "Campos from comp: Qnekt",
+      "purpose": "JustCampos from comp: Qnekt stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 51,
+      "title": "Jacobson from comp: Ziore",
+      "purpose": "JustJacobson from comp: Ziore stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 52,
+      "title": "Gross from comp: Futuris",
+      "purpose": "JustGross from comp: Futuris stuff todo",
+      "userId": 4
+    },
+    {
+      "id": 53,
+      "title": "Guy from comp: Earbang",
+      "purpose": "JustGuy from comp: Earbang stuff todo",
+      "userId": 4
     }
   ],
   'users': [
@@ -43,6 +331,10 @@ const STORAGE = {
     {
       'id': 3,
       'username': 'nowakp'
+    },
+    {
+      'id': 4,
+      'username': 'lot-of-items-johny'
     }
   ],
   'items': [
